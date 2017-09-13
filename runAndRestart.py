ec0cea79fac22c9c457cee632a2ee3668fe58cce
Simon Tucker <brotherlogic@gmail.com>
2017-09-12 20:03:39 -0700

    Forces a reboot

--- runAndRestart.py
+++ runAndRestart.py
@@ -5,5 +5,4 @@ os.popen('./syncer.sh').readlines()
 lines_after = len(os.popen('find ./target').readlines())
 
 if lines_before != lines_after:
-    print "Running!"
-    #os.popen('sudo reboot').readlines()
+    os.popen('sudo reboot').readlines()
