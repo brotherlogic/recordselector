8958245b619081ac489f31e297eb6d346cd01dcd
Simon Tucker <brotherlogic@gmail.com>
2017-09-12 20:34:03 -0700

    Wheee

--- runAndRestart.py
+++ runAndRestart.py
@@ -4,5 +4,6 @@ lines_before = os.popen('find ./target | grep jar-with').readlines()[0]
 os.popen('./syncer.sh').readlines()
 lines_after = len(os.popen('find ./target | grep jar-with').readlines()[0]
 
+print lines_before,lines_after
 if lines_before != lines_after:
     os.popen('sudo reboot').readlines()
