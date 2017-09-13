34007037bdfbee41da2ec897ecfbad4b5798e47d
Simon Tucker <simon@stationone>
2017-09-12 20:16:31 -0700

    Adds a build step

--- syncer.sh
+++ syncer.sh
@@ -2,4 +2,5 @@ git fetch -p
 git merge origin/master
 git gc
 rm recordselector.jar
+mvn clean package
 ln -s $(find target/ | grep jar-with-dependencies.jar$ | sort -n | tail -n 1) ./recordselector.jar
