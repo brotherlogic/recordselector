a129ec61a6947a6bef1b85a61c9cedd73a1a0065
Simon Tucker <brotherlogic@gmail.com>
2017-09-16 10:35:06 -0700

    Fixed the syner script for new compilation process

--- syncer.sh
+++ syncer.sh
@@ -2,5 +2,5 @@ git fetch -p
 git merge origin/master
 git gc
 rm recordselector.jar
-mvn clean package
-ln -s $(find target/ | grep jar-with-dependencies.jar$ | sort -n | tail -n 1) ./recordselector.jar
+rsync -avz --progress --delete-after 192.168.86.64:codestore/recordselector/ ./codestore
+ln -s $(find codestore/ | grep jar-with-dependencies.jar$ | sort -n | tail -n 1) ./recordselector.jar
