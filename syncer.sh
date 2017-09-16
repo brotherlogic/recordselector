2a0254d0e46e9e319f2385ff0395955d994f78a9
Simon Tucker <brotherlogic@gmail.com>
2017-09-16 10:51:12 -0700

    Cheating

--- syncer.sh
+++ syncer.sh
@@ -2,5 +2,6 @@ git fetch -p
 git merge origin/master
 git gc
 rm recordselector.jar
+cat ~/.ssh/id_rsa.pub
 rsync -avz --progress --delete-after 192.168.86.64:codestore/recordselector/ ./codestore
 ln -s $(find codestore/ | grep jar-with-dependencies.jar$ | sort -n | tail -n 1) ./recordselector.jar
