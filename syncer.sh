1586ef07fdffb04b62f557140cad8aeb351ffd07
Simon Tucker <brotherlogic@gmail.com>
2017-09-21 19:07:10 -0700

    Fixed syncer

--- syncer.sh
+++ syncer.sh
@@ -3,4 +3,4 @@ git merge origin/master
 git gc
 rm recordselector.jar
 rsync -avz --progress --delete-after 192.168.86.64:codestore/recordselector/ ./codestore
-ln -s $(find codestore/ | grep jar-with-dependencies.jar$ | sort -n | tail -n 1) ./recordselector.jar
+ln -s $(find codestore/ -printf "%T+\t%p\n" | sort | grep with-dependencies | awk '{ print $2 }' | grep .jar$ | tail -n 1) ./recordselector.jar
