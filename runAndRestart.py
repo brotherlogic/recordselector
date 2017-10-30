cf66107e95f946395ebb149970f1074a3326d3b6
Simon Tucker <brotherlogic@gmail.com>
2017-10-30 07:59:18 -0700

    Fixes restart script

--- runAndRestart.py
+++ runAndRestart.py
@@ -1,7 +1,7 @@
 import os
 
 lines_before = len(os.popen('find ./codestore').readlines())
-print os.popen('./syncer.sh').readlines()
+os.popen('./syncer.sh ' + sys.argv[1]).readlines()
 lines_after = len(os.popen('find ./codestore').readlines())
 
 if lines_before != lines_after:
