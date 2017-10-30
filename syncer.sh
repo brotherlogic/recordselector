e8cfb776bc9f879b5d5fddf5d44f3831ace7ffb5
Simon Tucker <brotherlogic@gmail.com>
2017-10-30 07:27:49 -0700

    Fixes rsync

--- syncer.sh
+++ syncer.sh
@@ -2,5 +2,5 @@ git fetch -p
 git merge origin/master
 git gc
 rm recordselector.jar
-rsync -avz --progress --delete-after 192.168.86.64:codestore/recordselector/ ./codestore
+rsync -e 'ssh -p 2223 -o StrictHostKeyChecking=no' -avz --delete-after --progress $(curl -s "$1"/resolve | awk -F ':' '{print $1}'):codestore/recordselector/ ./codestore
 ln -s $(find codestore/ -printf "%T+\t%p\n" | sort | grep with-dependencies | awk '{ print $2 }' | grep .jar$ | tail -n 1) ./recordselector.jar
