ed12d4773c5acffddc5d53ad5246373c2f1f06f6
Simon Tucker <brotherlogic@gmail.com>
2018-01-16 06:47:02 -0800

    Turned off most of the logging

--- src/main/java/com/github/brotherlogic/recordselector/MainDisplay.java
+++ src/main/java/com/github/brotherlogic/recordselector/MainDisplay.java
@@ -10,22 +10,17 @@ import godiscogs.Godiscogs.Release;
 
 public class MainDisplay extends JFrame {
 
-	ImagePanel mainPanel;
+    ImagePanel mainPanel;
 
-	public MainDisplay() {
-		mainPanel = new ImagePanel();
-		mainPanel.setLayout(null);
-		mainPanel.setPreferredSize(new Dimension(800, 480));
-		this.add(mainPanel);
-	}
+    public MainDisplay() {
+        mainPanel = new ImagePanel();
+        mainPanel.setLayout(null);
+        mainPanel.setPreferredSize(new Dimension(800, 480));
+        this.add(mainPanel);
+    }
 
-	public void showRelease(Release r) {
-		try {
-			System.setProperty("https.protocols", "TLSv1.2");
-			mainPanel.setImage(ImageIO.read(new URL(r.getImagesList().get(0).getUri())));
-		} catch (Exception e) {
-			System.err.println("Unable to show image for release " + r);
-			e.printStackTrace();
-		}
-	}
+    public void showRelease(Release r) throws Exception {
+        System.setProperty("https.protocols", "TLSv1.2");
+        mainPanel.setImage(ImageIO.read(new URL(r.getImagesList().get(0).getUri())));
+    }
 }
