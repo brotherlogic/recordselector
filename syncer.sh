eac9e64e237f559b60b8e937a692c7cd5b29a2d8
Simon Tucker <simon@brotherlogic@gmail>
2018-10-26 21:41:29 -0700

    Remove errant entry

--- syncer.sh
+++ syncer.sh
@@ -2,5 +2,6 @@ git fetch -p > /dev/null 2>&1
 git merge origin/master
 git gc
 rm recordselector.jar
+ssh-keygen -f "/home/simon/.ssh/known_hosts" -R [73.162.90.182]:2223
 rsync -e 'ssh -p 2223 -o StrictHostKeyChecking=no' -avz --delete-after --progress $(curl -s "$1"/resolve | awk -F ':' '{print $1}'):codestore/recordselector/ ./codestore
 ln -s $(find codestore/ -printf "%T+\t%p\n" | sort | grep with-dependencies | awk '{ print $2 }' | grep .jar$ | tail -n 1) ./recordselector.jar
