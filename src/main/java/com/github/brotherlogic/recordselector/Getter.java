0cdf7a885464b9183822a60526062805db280e26
Simon Tucker <simon@brotherlogic@gmail>
2019-09-27 09:38:47 -0700

    Increase timeout to 10 seconds

--- src/main/java/com/github/brotherlogic/recordselector/Getter.java
+++ src/main/java/com/github/brotherlogic/recordselector/Getter.java
@@ -26,8 +26,8 @@ public class Getter {
             if (refresh) {
                 System.err.println("Request refresh!");
             }
-            response = client.withDeadlineAfter(1, TimeUnit.SECONDS).getRecord(Recordgetter.GetRecordRequest.newBuilder().setRefresh(refresh).build());
-            channel.shutdown().awaitTermination(1, TimeUnit.SECONDS);
+            response = client.withDeadlineAfter(10, TimeUnit.SECONDS).getRecord(Recordgetter.GetRecordRequest.newBuilder().setRefresh(refresh).build());
+            channel.shutdown().awaitTermination(10, TimeUnit.SECONDS);
         }
         return response;
     }
